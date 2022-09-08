72d97ac9e587054b76c838a07c09df76532419e9
Sandro Pedrett <sandro.pedrett@hotmail.com>
2022-09-08 23:43:22 +0200

    group event identifiers

--- firmware/main/Events/EventIdentifiers.h
+++ firmware/main/Events/EventIdentifiers.h
@@ -16,10 +16,14 @@ struct EventIdentifiers {
 
     // DEVICE 60..79
     const static EventId DEVICE_SETTINGS_EVENT  = 60;
-    const static EventId SENSOR_DATA_EVENT      = 61;
-    const static EventId SENSOR_SNAPSHOT        = 62;
-    const static EventId SENSOR_STATUS          = 63;
-    const static EventId BTN_CTRL_EVENT         = 64;
+    const static EventId DEVICE_INFO_EVENT      = 61;
+    const static EventId BTN_CTRL_EVENT         = 62;
+
+    const static EventId SENSOR_DATA_EVENT      = 65;
+    const static EventId SENSOR_SNAPSHOT        = 66;
+    const static EventId SENSOR_STATUS          = 67;
+
+    const static EventId BATTERY_LEVEL_EVENT    = 70;
 
     // WIFI 80..99
     const static EventId WIFI_SETTINGS_EVENT    = 80;
